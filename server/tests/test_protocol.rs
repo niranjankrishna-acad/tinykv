3b93d244dac84d5dd272eacd16af2b7cba992e6a
Niranjan <niranjankrishna.acad@gmail.com>
2024-03-23 13:50:22 +0530

    error + number tests added

--- server/tests/test_protocol.rs
+++ server/tests/test_protocol.rs
@@ -4,11 +4,29 @@ use server::protocol;
 fn validate_type() {
 
     fn validate_string(){
-        let valid_data = b"+Hello\r\n";
-        let result = protocol::read(&valid_data.to_vec());
+        let valid_string = b"+Hello\r\n";
+        let result = protocol::read(&valid_string.to_vec());
         assert_eq!(result, 200);
-    }    
+    }
+
+    fn validate_error(){
+        let valid_error = b"-Error\r\n";
+        let result = protocol::read(&valid_error.to_vec());
+        assert_eq!(result, 200);
+    }
+
+    fn validate_number(){
+        let valid_number = b":32\r\n";
+        let result = protocol::read(&valid_number.to_vec());
+        assert_eq!(result, 200);
+
+        let invalid_number = b":abc\r\n";
+        let result = protocol::read(&invalid_number.to_vec());
+        assert_eq!(result, 400);
+    }
 
     validate_string();
+    validate_error();
+    validate_number();
 
 }
